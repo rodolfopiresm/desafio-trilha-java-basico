11cd0ca020d31e4c20c522e906bcd0bdbfc40c04
Rodolfo Pires <rodolfopiresm@gmail.com>
2026-03-26 08:11:44 -0300

    Ajustes na lógica para adequar ao exercício e também adicionado um comando novo (conta.nextLine();) depois de pesquisas, que permite eu digitar o nome completo do cliente

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -5,16 +5,20 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner conta = new Scanner(System.in).useLocale(Locale.US);
 
-        double saldo = 237.48;
-
         System.out.println("Por favor, digite o número da Agência !");
         String agencia = conta.next();
 
         System.out.println("Por favor, digite o número da Conta !");
         String numero = conta.next();
 
+        conta.nextLine(); // Em busca encontrei esse comando para limpar o buffer do scanner,
+        //pois o próximo comando é um nextLine() e ele estava pulando a leitura do nome do cliente.
+
         System.out.println("Por favor, digite o nome do titular da conta !");
-        String nomeCliente = conta.next();
+        String nomeCliente = conta.nextLine();
+
+        System.out.println("Por favor, digite o valor do depósito inicial !");
+        double saldo = conta.nextDouble();
 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
                 + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
